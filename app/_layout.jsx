ff62b266fb9c7b2145002bad7a90d3447216876b
[Aniket Bhosale] <[adbhosale2707]>
2024-06-11 11:56:50 +0530

    sign in and sign UI done

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,4 +1,4 @@
-import { SplashScreen,Slot } from "expo-router";
+import { SplashScreen, Stack } from "expo-router";
 import { Text } from "react-native";
 import {useFonts} from 'expo-font';
 import { useEffect } from "react";
@@ -30,7 +30,12 @@ const RootLayout = () => {
     <>
        
         
-        <Slot />
+        <Stack>
+          <Stack.Screen name="index" options={{headerShown:false}}/>
+          <Stack.Screen name="(auth)" options={{headerShown:false}}/>
+          <Stack.Screen name="(tabs)" options={{headerShown:false}}/>
+          {/* <Stack.Screen name="/search/[query]" options={{headerShown:false}}/> */}
+        </Stack>
         {/* contains all children routes */}
 
        
