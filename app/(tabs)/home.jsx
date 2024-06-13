d0a65c13b477177b26c2144748433c2e093c9f39
Aniket <bhosaleaniket826@gmail.com>
2024-06-13 11:20:13 +0530

    Profile page done

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -16,9 +16,10 @@ import EmptyState from "../../components/EmptyState";
 import { getAllPosts, getLatestPost } from "../../lib/appwrite";
 import useAppwrite from "../../lib/useAppwrite";
 import VideoCard from "../../components/VideoCard";
+import { useGlobalContext } from "../../context/GlobalProvider";
 
 const Home = () => {
-
+  const {user}=useGlobalContext()
   const {data:posts,refetch}=useAppwrite(getAllPosts);
   const {data:latestPosts}=useAppwrite(getLatestPost);
   
@@ -49,7 +50,7 @@ const Home = () => {
                   Welcome Back
                 </Text>
                 <Text className="text-2xl font-psemibold text-white">
-                  Aniket
+                  {user?.username}
                 </Text>
               </View>
               <View className="mt-1.5">
